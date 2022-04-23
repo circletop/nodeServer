2bf264a111983abb6ab74a8386aeb48594040f5d
lengxd <lengxd@unitechs.com>
2022-04-23 16:10:04 +0800

    展示已有接口到主页，增加控制台输出超链接

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,41 @@ const router = express.Router();
 // router.put('/get_user', userController.filterUser)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('../views/index', {title: '小伙伴',waiting: '敬请期待...'})
+/**
+ * [{
+ *   path: '',
+ *   method: '',
+ *   description: ''
+ * }]
+ */
+router.get('/', function (req, res, next) {
+  res.render('../views/index', {
+    title: '小伙伴',
+    waiting: '敬请期待...',
+    table: [{
+      path: '/users/lists',
+      method: 'post',
+      description: '查询人员列表'
+    },{
+      path: '/users//add',
+      method: 'post',
+      description: '增加人员'
+    },{
+      path: '/users/update/:id',
+      method: 'put',
+      description: '更新选中人员信息'
+    },{
+      path: '/users/del/:id',
+      method: 'delete',
+      description: '删除选中人员'
+    },{
+      path: '/users/profile/:id',
+      method: 'get',
+      description: '查询人员信息'
+    },]
+  })
 });
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   res.send({
     a: 1,
     b: 2
